194f9ceb024d0f3b324e80f2b8965ff487349018
Eity2021 <Eity2021akter@gmail.com>
2024-11-28 02:21:16 +0600

    add daisy ui

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Home() {
   return (
     <div >
-    Home
+ <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
   </div>
   )
 }
